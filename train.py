e7f8f0e0ddaddde48cf22e0434a57d2811bc2fa0
Michael Zheng <mhzbox@gmail.com>
2020-09-19 13:45:29 -0700

    detatch and train D and G nets separately

--- train.py
+++ train.py
@@ -6,15 +6,18 @@ from models import Generator, Discriminator
 from utils import init_weights
 
 # Parameters
-LEARNING_RATE = 0.2
+LEARNING_RATE = 2e-5
 SAVE_STEPS = 25
 BATCH_SIZE = 1
-SHAPE = (1, 1, 1, 1)
+SHAPE = (1, 1, 30, 30)
+
+loss_names = ['G_A_identity_loss', 'G_B_identity_loss', 'G_A_loss', 'G_B_loss', 'G_A_cycle_loss', 'G_B_cycle_loss']
 
 MSE_Loss = nn.MSELoss()
 L1_Loss = nn.L1Loss()
-real_target = torch.ones(SHAPE, device=try_gpu(), requires_grad=False)
-fake_target = torch.zeros(SHAPE, device=try_gpu(), requires_grad=False)
+
+D_real_target = torch.ones(SHAPE, device=try_gpu(), requires_grad=False)
+D_fake_target = torch.zeros(SHAPE, device=try_gpu(), requires_grad=False)
 
 # Create computation graphs for Models
 # G_A is Generator that maps from A domain to B domain
@@ -41,9 +44,9 @@ adam_cycle = torch.optim.Adam(chain(G_A.parameters(), G_B.parameters()), lr=LEAR
 adam_D_A = torch.optim.Adam(D_A.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
 adam_D_B = torch.optim.Adam(D_B.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
 
-lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
-lr_scheduler_D_A = torch.optim.lr_scheduler.LambdaLR(optimizer_D_A, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
-lr_scheduler_D_B = torch.optim.lr_scheduler.LambdaLR(optimizer_D_B, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
+# lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
+# lr_scheduler_D_A = torch.optim.lr_scheduler.LambdaLR(optimizer_D_A, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
+# lr_scheduler_D_B = torch.optim.lr_scheduler.LambdaLR(optimizer_D_B, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
 
 def cycle_update(G_A, G_B, D_A, D_B, A_img, B_img, optimizer):
     optimizer.zero_grad()
@@ -58,12 +61,15 @@ def cycle_update(G_A, G_B, D_A, D_B, A_img, B_img, optimizer):
     # Generator Loss
     # Generator is trying to trick Discriminator labeling generated image as 1
     fake_B = G_A(A_img)
-    fake_B_output = D_B(fake_B)
-    fake_B_loss = MSE_Loss(fake_B_output, real_target)
-
     fake_A = G_B(B_img)
-    fake_A_output = D_A(fake_A)
-    fake_A_loss = MSE_Loss(fake_A_output, real_target)
+    
+    # Don't update Discriminator when updating Generator
+    with torch.no_grad():
+        fake_B_output = D_B(fake_B)
+        fake_A_output = D_A(fake_A)
+    
+    fake_B_loss = MSE_Loss(fake_B_output, D_real_target)
+    fake_A_loss = MSE_Loss(fake_A_output, D_real_target)
 
     # Cycle Loss
     cycle_A_output = G_B(fake_B)
@@ -72,16 +78,16 @@ def cycle_update(G_A, G_B, D_A, D_B, A_img, B_img, optimizer):
     cycle_B_output = G_A(fake_A)
     cycle_B_loss = MSE_Loss(cycle_B_output, B_img)
 
-    total_loss = AA_identity_loss + BB_identity_loss + fake_B_loss + fake_A_loss + cycle_A_loss + cycle_B_loss
+    total_loss = A_identity_loss + B_identity_loss + fake_B_loss + fake_A_loss + cycle_A_loss + cycle_B_loss
     total_loss.backward()
     optimizer.step()
-    return [AA_identity_loss, BB_identity_loss, fake_A_loss, fake_B_loss, cycle_A_loss, cycle_B_loss]
+    return [A_identity_loss, B_identity_loss, fake_A_loss, fake_B_loss, cycle_A_loss, cycle_B_loss]
 
 def discriminator_update(D, real_imgs, fake_imgs, optimizer):
     optimizer.zero_grad()
     fake_output = D(fake_imgs)
     real_output = D(real_imgs)
-    discriminator_loss = (MSE_Loss(real_output, real_target) + MSE_Loss(fake_output, fake_target)) * 0.5
+    discriminator_loss = (MSE_Loss(real_output, D_real_target) + MSE_Loss(fake_output, D_fake_target)) * 0.5
     discriminator_loss.backward()
     optimizer.step()
     return discriminator_loss
@@ -91,7 +97,7 @@ for e in range(num_epochs):
         # Cycle Update
         cycle_update_losses = cycle_update(G_A, G_B, D_A, D_B, A_img, B_img, adam_cycle)
         
-        # Creake Fake Image to update Discriminator
+        # Create Fake Image to update Discriminator
         fake_A = G_B(B_img).detach()
         D_A_loss = discriminator_update(D_A, A_img, fake_A, adam_D_A)
         fake_B = G_A(A_img).detach()
